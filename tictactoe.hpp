e675b86479d709982b656633d897bed68fcfce66
fanfreak2247 <keandrewinston@gmail.com>
2019-12-29 19:30:14 -0500

    Update tictactoe.hpp

--- tictactoe.hpp
+++ tictactoe.hpp
@@ -13,7 +13,7 @@ extern "C"{
 class tictactoe{
 	private:
 	std::array<char,9> board={'1','2','3','4','5','6','7','8','9'};
-	std::vector<char> playSymbols={'X','O','@','$','%','&'}; //@, $, % and &			
+	std::vector<char> playSymbols={'X','O','@','$','%','&'};		
 	char playerOne;
 	char playerTwo;
 
